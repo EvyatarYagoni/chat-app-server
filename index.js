f91dadc49cf86139708946591faa8ae512dc87d3
Evyatar Yagoni <evyatar@hackeruso.com>
2023-07-09 10:18:06 +0300

    feat: try to separate routes folder

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const dotenv = require('dotenv');
-dotenv.config();
+const cors = require('cors');
 
-const port = process.env.PORT || 3001;
+dotenv.config();
+app.use(cors());
+
+const port = process.env.PORT || 8080;
 app.get('/', function(req, res) {
     res.send('Hello World!');
 });
+
+app.get('/health', function(req, res) {
+    res.send('OK');
+});
+
 server.listen(port, function() {
     console.log(`Listening on port ${port}`);
 });
