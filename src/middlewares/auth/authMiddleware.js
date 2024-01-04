f00e63b3106e1b770ce10acdf2282d3f0c02d262
Evyatar Yagoni <evyatar@hackeruso.com>
2024-01-04 18:52:48 +0200

    feat: update auth middleware

--- src/middlewares/auth/authMiddleware.js
+++ src/middlewares/auth/authMiddleware.js
@@ -1,12 +1,25 @@
 const jwt = require('jsonwebtoken');
 
 const authenticateTokenMiddleware = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; // Bearer <token>
-    if (token == null) return res.sendStatus(401);
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
+    const accessToken = req.cookies.accessToken;
+
+    if (!accessToken) {
+        console.log('No access token');
+        return res.sendStatus(401);
+    }
+
+    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(403);
+        }
+
         req.user = user;
+
+        console.log('user', user);
+
+
+
         next();
     });
 };
