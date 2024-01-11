6f70634d25949dd59202f3884d4d4b3751717ddf
Evyatar Yagoni <evyatar@hackeruso.com>
2024-01-11 19:39:35 +0200

    feat(CW-Login) fix verifyJWT middleware

--- src/middlewares/auth/verifyJWTMiddleware.js
+++ src/middlewares/auth/verifyJWTMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const verifyJWTMiddleware = (req, res, next) => {
     const authHeader = req.headers['authorization'];
-    const isBarer = authHeader?.startsWith('Barer ');
+    const isBarer = authHeader?.startsWith('Bearer ');
 
     if (!authHeader || !isBarer) {
         return res.status(401).send({ message: 'Unauthorized' });
@@ -11,11 +11,13 @@ const verifyJWTMiddleware = (req, res, next) => {
     const token = authHeader?.split(' ')[1];
 
     if (!token) {
+        console.log('!token', token);
         return res.status(401).send({ message: 'Unauthorized' });
     }
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if (err) {
+            console.log('err', err);
             return res.status(401).send({ message: 'Unauthorized' });
         }
         req.user = user;
