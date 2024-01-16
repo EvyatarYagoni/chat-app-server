5004c93cff973788295fd78ebf86a91c5dcd2e56
Evyatar Yagoni <evyatar@hackeruso.com>
2024-01-16 15:43:55 +0200

    feat(CW-NCM) add protected routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const authRoutes = require('./auth/auth');
 const publicRoutes = require('./public/public');
+const protectedRoutes = require('./protected/protected');
+const verifyJWTMiddleware = require('../middlewares/auth/verifyJWTMiddleware');
 
-// Authentication routes
 router.use('/auth', authRoutes);
 router.use('', publicRoutes);
+router.use('', verifyJWTMiddleware ,protectedRoutes);
 
 module.exports = router;
