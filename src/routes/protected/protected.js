72e2f2f2f7de7186f60c434a5b3d8e66b4cf3bb4
Evyatar Yagoni <evyatar@hackeruso.com>
2024-01-17 15:11:49 +0200

    feat(CW-NCM) add message and chat routes

--- src/routes/protected/protected.js
+++ src/routes/protected/protected.js
@@ -1,8 +1,13 @@
 const {Router} = require('express');
 const router = Router();
 const userRoutes = require('./user/user');
+const messageRoutes = require('./message/messageRoutes');
+const chatRoutes = require('./chat/chatRoutes');
 
 
 router.use('/users', userRoutes);
+router.use('/message', messageRoutes);
+router.use('/chat', chatRoutes);
+
 
 module.exports = router;
